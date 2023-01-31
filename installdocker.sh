6da9fe0f3a4f367becdc242fef172a33e4260cf7
Indian-Techie09 <102227305+Indian-Techie09@users.noreply.github.com>
2023-01-31 23:16:43 +0530

    Add files via upload

---
+++ installdocker.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Docker and compose install
+sudo apt-get update 
+sudo apt-get upgrade -y
+sudo apt-get install \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo service docker start
+echo docker is starting
+sleep 10
+sudo systemctl enable docker.service
+sudo docker run hello-world
+sudo docker -v
+sudo docker-compose -v
+echo docker and compose installed

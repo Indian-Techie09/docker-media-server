fcf869ad3b9bc9776d521d1a4cbba9ef0ae535be
Indian-Techie09 <102227305+Indian-Techie09@users.noreply.github.com>
2023-01-31 22:38:27 +0530

    Update docker-media-server.sh

--- docker-media-server.sh
+++ docker-media-server.sh
@@ -2,8 +2,8 @@
 # Docker Media Server
 sudo apt-get update 
 sudo apt-get upgrade -y
-mkdir dockere
-cd dockere
+mkdir docker
+cd docker
 mkdir appdata
 mkdir custom
 mkdir logs
@@ -19,10 +19,10 @@ wget -O $PWD/.env https://raw.githubusercontent.com/Indian-Techie09/docker-media
 sleep 5
 cd ..
 sudo apt install acl
-sudo chmod 775 /home/$USER/dockere
-sudo setfacl -Rdm g:docker:rwx /home/$USER/dockere
-sudo setfacl -Rm g:docker:rwx /home/$USER/dockere
-cd dockere
+sudo chmod 775 /home/$USER/docker
+sudo setfacl -Rdm g:docker:rwx /home/$USER/docker
+sudo setfacl -Rm g:docker:rwx /home/$USER/docker
+cd docker
 sudo chown root:root .env
 sudo chmod 600 .env
 sudo nano .env

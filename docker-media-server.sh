b97a6a3548aedac0141e6ea3a044a5627e4b16e9
Indian-Techie09 <102227305+Indian-Techie09@users.noreply.github.com>
2023-05-07 20:04:03 +0530

    Update docker-media-server.sh

--- docker-media-server.sh
+++ docker-media-server.sh
@@ -12,17 +12,8 @@ mkdir logs
 mkdir secrets
 mkdir shared
 mkdir scripts
-sleep 5
-wget -O $PWD/docker-compose.yml https://raw.githubusercontent.com/Indian-Techie09/docker-media-server/main/docker-compose.yml
-sleep 5
-wget -O $PWD/.env https://raw.githubusercontent.com/Indian-Techie09/docker-media-server/main/.env
-sleep 5
 cd ..
 sudo apt install acl
 sudo chmod 775 /home/$USER/docker
 sudo setfacl -Rdm g:docker:rwx /home/$USER/docker
 sudo setfacl -Rm g:docker:rwx /home/$USER/docker
-cd docker
-sudo chown root:root .env
-sudo chmod 600 .env
-sudo nano .env
